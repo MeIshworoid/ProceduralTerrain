d388dc6786062ea478d245880b2af8b94ed50c6b
Im-NoBoDY <ishwordhungana894@gmail.com>
2025-05-03 17:51:52 +0545

    Update FallOffGenerator to create a 2D falloff map array instead of 1D, preparing for further map generation logic.

--- Assets/Scripts/FallOffGenerator.cs
+++ Assets/Scripts/FallOffGenerator.cs
@@ -6,6 +6,14 @@ public static class FallOffGenerator
 {
     public static float[,] GenerateFalloffMap(int size)
     {
-        float[,] map = new float[size];
+        float[,] map = new float[size, size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                float x = i / (size);
+            }
+        }
     }
 }
